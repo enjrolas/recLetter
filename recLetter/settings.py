9e359f37052b8e46f4d888a1b44f1076f060f2d5
japhy <japhy@li414-198.members.linode.com>
2013-04-12 06:14:50 -0400

    everything's looking pretty good

--- recLetter/settings.py
+++ recLetter/settings.py
@@ -53,7 +53,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://recletter.artiswrong.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -63,7 +63,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://recletter.artiswrong.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -99,6 +99,8 @@ MIDDLEWARE_CLASSES = (
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+ROOT_URL="http://recletter.artiswrong.com/"
 
 ROOT_URLCONF = 'recLetter.urls'
 
